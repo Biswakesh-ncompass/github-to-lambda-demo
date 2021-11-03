8b647318a5e8ab9b27beb2adfb39065c8a0a0666
biswakesh-ottosero <biswakesh@ottosero.com>
2021-11-03 19:40:35 +0530

    added numpy layer

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import numpy as np
 
 
 def lambda_handler(event, context):
     d={'col1':[1,2],'col2':[3,4],'col3':[5,6]}
+    a=[[1,2]]
+    b=[[3,4]]
+    matrix=np.add(a,b)
     df=pd.DataFrame(data=d)
     print(df)
     print('Done X1.2')
+    print(matrix)
